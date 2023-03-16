bf6e7aed64b3f290920997ddcfb57bde9712a712
PinoMacri <pinonegozio1@outlook.it>
2023-03-16 17:45:24 +0100

    Esercizio Conseganto - Dettaglio Definito

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource("projects", ProjectController::class);
+Route::get("/projects/{project}", [ProjectController::class,"show"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
